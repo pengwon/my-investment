7090bd05d338f77e0883d8a4343a4a3ae54fc89d
peter <peter@boringhex.top>
2024-02-19 23:13:11 +0800

    update script

--- my-investment/portfolio/portfolio-1.py
+++ my-investment/portfolio/portfolio-1.py
@@ -2,7 +2,9 @@ import requests
 import json
 import time
 import re
+from bs4 import BeautifulSoup
 from datetime import datetime
+
 
 def is_trade_day(date: str):
     # 读取 JSON 文件
@@ -13,10 +15,13 @@ def is_trade_day(date: str):
     current_year = datetime.now().strftime("%Y")
 
     # 检查当前日期是否在文件中
-    if current_year in data and any(item["t_dt"] == date for item in data[current_year]):
+    if current_year in data and any(
+        item["t_dt"] == date for item in data[current_year]
+    ):
         return True
     else:
         return False
+
 
 def update_index_data(index_code: str, date: str):
     timestamp_ms = int(time.time() * 1000)
@@ -56,7 +61,7 @@ def update_index_data(index_code: str, date: str):
         "change_percent": (data["f43"] - data["f60"]) / data["f60"] * 100,
         "trading_volume": data["f47"],
         "trading_value": data["f48"],
-        "turnover_rate": data["f47"] * 100 / data["f85"] * 100
+        "turnover_rate": data["f47"] * 100 / data["f85"] * 100,
     }
     # Add your code here to update the index data
     file_path = f"../data/{index_code}.json"
@@ -77,11 +82,52 @@ def update_index_data(index_code: str, date: str):
         json.dump(index_data, f)
         f.truncate()
 
+
 def update_fund_data(fund_code: str, date: str):
+    url = f"https://fund.eastmoney.com/{fund_code}.html"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
+    }
+    response = requests.get(url, headers=headers)
+    html = response.text
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    data_item_02 = soup.find("dl", {"class": "dataItem02"})
+    data_date = data_item_02.find("dt").text.split(" ")[-1].strip("()")
+    unit_value = data_item_02.find(
+        "span", {"class": re.compile(r"ui-font-large ui-color-(red|green) ui-num")}
+    ).text
+    growth_rate = data_item_02.find_all(
+        "span", {"class": re.compile(r"ui-font-middle ui-color-(red|green) ui-num")}
+    )[0].text
+
+    if data_date == date:
+        new_data = {"date": date, "unit_value": unit_value, "growth_rate": growth_rate}
+        file_path = f"../data/{fund_code}.json"
+        with open(file_path, "r+", encoding="utf-8") as f:
+            fund_data = json.load(f)
+            # 查找给定日期的数据
+            for item in fund_data["market_data"]:
+                if item["date"] == date:
+                    # 如果找到了，更新数据
+                    item.update(new_data)
+                    break
+            else:
+                # 如果没有找到，添加新的数据
+                fund_data["market_data"].append(new_data)
+
+            # 将修改后的数据写回文件
+            f.seek(0)
+            json.dump(fund_data, f)
+            f.truncate()
+
+
+def get_fund_data(fund_code: str, date: str):
     if fund_code != "014533":
         url = f"https://fundgz.1234567.com.cn/js/{fund_code}.js"
         headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
         }
         print(url)
         response = requests.get(url, headers=headers)
@@ -89,19 +135,18 @@ def update_fund_data(fund_code: str, date: str):
         response_text = response.content.decode("utf-8")
         print(response_text)
         # 使用正则表达式删除函数调用
-        json_text = re.sub(r'^\w+\((.*)\);$', r'\1', response_text)
+        json_text = re.sub(r"^\w+\((.*)\);$", r"\1", response_text)
 
         # 解析 JSON 数据
         data = json.loads(json_text)
         print(data)
-
         new_data = {
             "date": date,
             "gsz": data["gsz"],
             "gszzl": data["gszzl"],
             "gsz_time": data["gztime"],
             "jzrq": data["jzrq"],
-            "dwjz": data["dwjz"]
+            "dwjz": data["dwjz"],
         }
 
         file_path = f"../data/{fund_code}.json"
@@ -122,6 +167,7 @@ def update_fund_data(fund_code: str, date: str):
             json.dump(fund_data, f)
             f.truncate()
 
+
 def update_market_data():
     # 获取当前日期
     current_date = datetime.now().strftime("%Y-%m-%d")
@@ -130,7 +176,7 @@ def update_market_data():
 
         with open("../data/portfolio-1.json", "r") as file:
             data = json.load(file)
-        
+
         # 获取 index_code 和 fund_code
         index_codes = data["index_code"]
         fund_codes = data["fund_code"]
@@ -138,12 +184,11 @@ def update_market_data():
         # 对于每个 index_code，修改相应的 JSON 文件
         for index_code in index_codes:
             update_index_data(index_code, current_date)
-            
+
         # 对于每个 fund_code，修改相应的 JSON 文件
         for fund_code in fund_codes:
             update_fund_data(fund_code, current_date)
-            
 
-    
+
 if __name__ == "__main__":
     update_market_data()
